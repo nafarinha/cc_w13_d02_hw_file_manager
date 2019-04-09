48f710508d54b43bc2dd0c574fc13eb9c3a72900
Nuno Farinha <nadfarinha@gmail.com>
2019-04-09 17:44:12 +0100

    Create models

--- src/test/java/com/example/spring_hw_file_manager/hw_spring_file_manager/HwSpringFileManagerApplicationTests.java
+++ src/test/java/com/example/spring_hw_file_manager/hw_spring_file_manager/HwSpringFileManagerApplicationTests.java
@@ -1,5 +1,8 @@
 package com.example.spring_hw_file_manager.hw_spring_file_manager;
 
+import com.example.spring_hw_file_manager.hw_spring_file_manager.models.File;
+import com.example.spring_hw_file_manager.hw_spring_file_manager.models.Folder;
+import com.example.spring_hw_file_manager.hw_spring_file_manager.models.User;
 import com.example.spring_hw_file_manager.hw_spring_file_manager.repositories.FileRepository;
 import com.example.spring_hw_file_manager.hw_spring_file_manager.repositories.FolderRepository;
 import com.example.spring_hw_file_manager.hw_spring_file_manager.repositories.UserRepository;
@@ -14,15 +17,31 @@ import org.springframework.test.context.junit4.SpringRunner;
 public class HwSpringFileManagerApplicationTests {
 
 	@Autowired
-	FileRepository fileRepository;
+	UserRepository userRepository;
 
 	@Autowired
 	FolderRepository folderRepository;
 
 	@Autowired
-	UserRepository userRepository;
+	FileRepository fileRepository;
+
+
 	@Test
 	public void contextLoads() {
 	}
+
+	@Test
+	public void canCreateFileManager() {
+		User user = new User("Travis");
+		userRepository.save(user);
+
+		Folder folder = new Folder("Documents", user);
+		folderRepository.save(folder);
+
+		File file = new File("thesis", ".doc", folder);
+		fileRepository.save(file);
+	}
+
+
 
 }
